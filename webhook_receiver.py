6f77e40392d0ac062bcd94429cebcb3d84d56dbd
RavindranadhTagore <137029385+RavindranadhTagore@users.noreply.github.com>
2025-04-28 23:48:16 +0530

    Create webhook_receiver.py

---
+++ webhook_receiver.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/alert', methods=['POST'])
+def alert():
+    data = request.json
+    print("Received alert:", data)
+    os.system("ansible-playbook restart_nginx.yml")
+    return 'Alert received', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
